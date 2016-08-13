260deedf346c1272bbd248ddcdeed902f3a2ddde
Niels NTG <NTG999@gmail.com>
2016-08-13 20:55:49 +0200

    Removed unneeded code from screen 2

--- screens/2/js/main.js
+++ screens/2/js/main.js
@@ -1,42 +1,6 @@
 // setup global vars
 var TSPSConnection;
-//sequencer stuff
-var drumpreset, drumsetup, drumcomp;
-//initialising the sequencer, remeber this is async, so you'll want to wait before just calling
-//things inside the sequencer
-var app = angular.module('9095App').directive('test', function (presetStorage, setup, sequencer) {
-    return {
-        link: function () {
-            console.log("booyah");
-            //Hier alle logica indrukken
-            (function() {
-                drumsetup = setup;
-                drumpreset = presetStorage;
-                drumcomp = sequencer;
-                for (var i = 0; i <= 15; i++) {
-                    //Een functie voor elke tick (0-15 ticks, 4 maten)
-                    $('body').scope().$on('tick_' + i, function (event, data) {
-                        console.log(event)
-                    });
-                }
-            });
-        }
-    }
-});
 
-function testGlobal() {
-    //We should prob create a function, i another file, that
-    //sets the different patterns
-    drumcomp.setSelectedInstrument('hand_clap');
-    drumcomp.setStep(1, 1);
-    drumcomp.setStep(2, 1);
-    drumcomp.setStep(3, 1);
-    //                        We should check if startPlay should be called again after each
-    //                            change to the drumcomputer
-    //                        console.log("tempo",drumcomp.getTempo());
-    drumcomp.startPlay(true);
-}
-var peopleArray = [];
 var socket = io('ws://192.168.1.124:4000');
 
 var portraitArray = [];
@@ -222,38 +186,8 @@ $(document).ready(function () {
     // TSPSConnection = new TSPS.Connection( their IP, their TSPS port )
     TSPSConnection.connect();
     // add listeners
-    TSPSConnection.onPersonEntered = onPersonEntered;
-    TSPSConnection.onPersonMoved = onPersonMoved;
-    TSPSConnection.onPersonUpdated = onPersonUpdated;
-    TSPSConnection.onPersonLeft = onPersonLeave;
+    // TSPSConnection.onPersonEntered = onPersonEntered;
+    // TSPSConnection.onPersonMoved = onPersonMoved;
+    // TSPSConnection.onPersonUpdated = onPersonUpdated;
+    // TSPSConnection.onPersonLeft = onPersonLeave;
 });
-
-function updateCanvas(person) {
-    // console.log(person);
-}
-
-function onPersonEntered(person) {
-    person.personID = peopleArray.length + 1;
-    peopleArray.push(person);
-    //    console.log(peopleArray.length);
-    updateCanvas(person);
-    socket.emit('application.message', peopleArray.length);
-}
-
-function onPersonUpdated(person) {
-    //    console.log('person updated')
-    updateCanvas(person);
-}
-
-function onPersonMoved(person) {
-    //    console.log('person moved');
-    updateCanvas(person);
-}
-
-function onPersonLeave(person) {
-    peopleArray = peopleArray.filter(function (obj) {
-        return obj.id !== person.id;
-    });
-    //    console.log(peopleArray.length);
-    socket.emit('application.message', peopleArray.length);
-}
